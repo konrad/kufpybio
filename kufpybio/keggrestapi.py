e331f28efa156b410847ce97a8b72e40ce1d8341
Konrad Förstner <konrad@foerstner.org>
2013-01-18 14:19:21 +0100

    Use REST API abstraction in KEGGRESTAPI

--- kufpybio/keggrestapi.py
+++ kufpybio/keggrestapi.py
@@ -1,10 +1,11 @@
 # http://www.kegg.jp/kegg/docs/keggapi.html
 
 import os
-import urllib.request
 import csv
 
-class KEGGRESTAPI(object):
+import restapi
+
+class KEGGRESTAPI(restapi.RESTAPI):
 
     def __init__(self, download_folder="kegg_files"):
         self._download_folder = download_folder
@@ -38,25 +39,6 @@ class KEGGRESTAPI(object):
 
     def _clean_list(self, id_list, prefix_to_remove):
         return([item.replace(prefix_to_remove, "") for item in id_list])
-
-    def _get_data(self, path_template, url_template, entity_id):
-        file_path = self._file_path(path_template, entity_id)
-        if not os.path.exists(file_path):
-            self._retrive_data(url_template, entity_id, file_path)
-        return(open(file_path).read())
-
-    def _retrive_data(self, url_template, entity_id, file_path):
-        data = urllib.request.urlopen(
-            self._base_url + url_template % (entity_id)).read()
-        data_fh = open(file_path, "wb")
-        data_fh.write(data)
-        data_fh.close()
-
-    def _file_path(self, path_template, entity_id):
-        return(path_template % (self._download_folder, entity_id))
-
-    def _rest_url(self, url_template, entity_id):
-        return(url_template % (self._base_url, entity_id))
 
     def _second_column_to_list(self, data):
         return(
