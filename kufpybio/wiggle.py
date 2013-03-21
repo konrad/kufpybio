30a567e6b4cf4e8bb7d53b2e19a9424c75821091
Konrad Förstner <konrad@foerstner.org>
2013-03-21 12:33:55 +0100

    Add WiggleWriter

--- kufpybio/wiggle.py
+++ kufpybio/wiggle.py
@@ -9,7 +9,7 @@ class WiggleParser(object):
 
     def entries(self, input_fh):
         track_name = None
-        chrom_name = None
+        replicon = None
         span = None
         pos_value_pairs = []
         for line in input_fh:
@@ -19,22 +19,22 @@ class WiggleParser(object):
             if row[0].startswith("track"):
                 track_name = self._track_name(row)
             elif row[0].startswith("variableStep"):
-                if chrom_name:
-                    prev_chrom_name = chrom_name
+                if replicon:
+                    prev_replicon = replicon
                     prev_span = span
                     prev_pos_value_pairs = pos_value_pairs
-                    chrom_name = self._chrom_name(row)
+                    replicon = self._replicon(row)
                     span = None
                     pos_value_pairs = []
                     yield(WiggleEntry(
-                            track_name, prev_chrom_name, prev_span, prev_pos_value_pairs))
+                            track_name, prev_replicon, prev_span, prev_pos_value_pairs))
                 else:
-                    chrom_name = self._chrom_name(row)
+                    replicon = self._replicon(row)
             else:
                 pos_value_pairs.append([int(row[0]), float(row[1])])
-        yield(WiggleEntry(track_name, chrom_name, span, pos_value_pairs))
+        yield(WiggleEntry(track_name, replicon, span, pos_value_pairs))
 
-    def _chrom_name(self, row):
+    def _replicon(self, row):
         return(self._attrs_and_values(row)["chrom"])
 
     def _track_name(self, row):
@@ -52,8 +52,29 @@ class WiggleParser(object):
 
 class WiggleEntry(object):
 
-    def __init__(self, track_name, chrom_name, span, pos_value_pairs):
+    def __init__(self, track_name, replicon, span, pos_value_pairs):
         self.track_name = track_name
-        self.chrom_name = chrom_name
+        self.replicon = replicon
         self.span = span
         self.pos_value_pairs = pos_value_pairs
+
+class WiggleWriter(object):
+
+    def __init__(self, track_str, fh):
+        self._fh = fh
+        self._fh.write(("track type=wiggle_0 name=\"%s\"\n" % (track_str)))
+
+    def write_replicons_coverages(
+            self, replicon, pos_value_pairs, factor=1.0):
+            self._fh.write("variableStep chrom=%s span=1\n" % (replicon))
+            # Filter values of 0 and multiply the remaining ones by
+            # the given factor.
+            self._fh.write(
+                "\n".join(["%s %s" % (pos, coverage * factor)
+                           for pos, coverage in
+                           filter(lambda pos_and_cov: pos_and_cov[1] != 0.0,
+                                  pos_value_pairs)]) + "\n")
+
+    def close_file(self):
+        self._fh.close()
+
