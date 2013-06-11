170545f4daafecb9bd8997494b796b995eb4702c
Konrad Förstner <konrad@foerstner.org>
2013-06-11 10:30:08 +0200

    Add GTF lib

---
+++ kufpybio/gtf.py
@@ -0,0 +1,24 @@
+from gff3 import Gff3Parser, Gff3Entry
+
+class GtfParser(Gff3Parser):
+    
+    def _dict_to_entry(self, entry_dict):
+        return(GtfEntry(entry_dict))
+
+class GtfEntry(Gff3Entry):
+    
+    def _strip(self, string):
+        return(string.strip())
+
+    def _attributes(self, attributes_string):
+        """Translate the attribute string to dictionary"""
+
+        attributes_string = attributes_string.rstrip(";")
+        return(dict(
+                [self._strip(key_value_pair).split(" ") 
+                 for key_value_pair in attributes_string.split(";")]))
+
+    def add_attribute(self, key, value):
+        self.attributes[key] = value
+        self.attribute_string = "; ".join(
+            [" ".join(items) for items in self.attributes.items()])
