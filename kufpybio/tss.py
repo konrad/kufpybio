77ea926c854d89768e992fdce628663f21fa7dab
Konrad Förstner <konrad@foerstner.org>
2013-07-12 09:23:20 +0200

    Add __repr__ to TSS

--- kufpybio/tss.py
+++ kufpybio/tss.py
@@ -17,3 +17,10 @@ class TSS(object):
         self.strand = strand
         if extra:
             self.extra = extra
+
+    def __repr__(self):
+        extra_str = ""
+        if not self.extra is None:
+            extra_str = self.extra
+        return "TSS: replicon: %s pos: %s strand: %s extra: %s" % (
+            self.seq_id, self.pos, self.strand, extra_str)
